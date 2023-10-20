d9376054ea376a7f23f2dde36102f12edbbd7c9a
dev2-nomo <dev2@nomo.app>
2023-10-20 11:53:25 +0200

    fix lint

--- example/lib/nomo_app.dart
+++ example/lib/nomo_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
 import 'package:nomo_router/nomo_router.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
