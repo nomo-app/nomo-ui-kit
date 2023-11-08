eeee09d8f974b27c1c9b4a0491cff70c4f10980c
dev2-nomo <dev2@nomo.app>
2023-11-08 14:46:23 +0100

    new implementation of the NomoDialog

--- lib/components/dialog/nomo_dialog.dart
+++ lib/components/dialog/nomo_dialog.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:nomo_ui_kit/components/button/button_const.dart';
 import 'package:nomo_ui_kit/components/button/nomo_button.dart';
-import 'package:nomo_ui_kit/components/dialog/dialog_const.dart';
+import 'package:nomo_ui_kit/components/text/nomo_text.dart';
+import 'package:nomo_ui_kit/theme/nomo_theme.dart';
 
 class NomoDialog extends StatelessWidget {
   final String title;
   final TextStyle? titleStyle;
   final Color? backgroundColor;
-  final List<Widget> content;
+  final Widget content;
   final List<Widget> actions;
   final BorderSide? border;
   final double? elevation;
-  final double? width;
-  final double? height;
   final bool? showCloseButton;
+  final double? widthRatio;
   final NomoButton? closeButton;
+  final EdgeInsetsGeometry margin;
   final EdgeInsetsGeometry? padding;
   final BorderRadiusGeometry? borderRadius;
   final Color? closeButtonColor;
   final Color? closeButtonIconColor;
+  final double? maxWidth;
+  final double? maxHeight;
 
   const NomoDialog({
     super.key,
@@ -26,90 +28,113 @@ class NomoDialog extends StatelessWidget {
     required this.actions,
     required this.content,
     this.showCloseButton = true,
-    this.border = DialogConst.border,
+    this.border,
     this.closeButton,
-    this.elevation = DialogConst.elevation,
-    this.borderRadius = DialogConst.borderRadius,
-    this.titleStyle = DialogConst.titleStyle,
-    this.padding = DialogConst.padding,
-    this.closeButtonColor = DialogConst.closeButtonColor,
-    this.closeButtonIconColor = DialogConst.closeButtonIconColor,
-    this.width,
-    this.height,
-    this.backgroundColor = const Color(0xFFFFFFFF),
+    this.elevation = 2,
+    this.borderRadius = const BorderRadius.all(
+      Radius.circular(12.0),
+    ),
+    this.titleStyle,
+    this.widthRatio = 0.75,
+    this.padding = const EdgeInsets.all(12.0),
+    this.margin = const EdgeInsets.only(bottom: 16),
+    this.closeButtonColor,
+    this.closeButtonIconColor,
+    this.maxWidth,
+    this.maxHeight,
+    this.backgroundColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        shape: RoundedRectangleBorder(
-          side: border!,
-          borderRadius: borderRadius!,
-        ),
-        elevation: elevation!,
-        color: backgroundColor,
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight: height ?? DialogConst.height,
-            maxWidth: width ?? DialogConst.width,
-          ),
-          child: Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              if (showCloseButton!)
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: closeButton == null
-                      ? NomoButton.icon(
-                          padding: const EdgeInsets.all(0),
-                          color: closeButtonColor!,
-                          elevation: 0,
-                          size: ButtonSize.small,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(100.0),
-                          ),
-                          onPressed: () => Navigator.pop(context),
-                          icon: Icon(
-                            Icons.close,
-                            color: closeButtonIconColor,
-                          ),
-                        )
-                      : closeButton!,
-                ),
-              Positioned(
-                top: 30,
-                child: Text(
-                  title,
-                  style: titleStyle!,
-                ),
-              ),
-              Positioned(
-                top: 50,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: Padding(
-                  padding: padding!,
+    final double width = MediaQuery.of(context).size.width * widthRatio!;
+    final double height = MediaQuery.of(context).size.height *
+        MediaQuery.of(context).systemGestureInsets.top;
+    final double rowWidth = width - padding!.horizontal - 100;
+
+    return SizedBox(
+      height: height,
+      child: Column(
+        children: [
+          Expanded(
+              child: Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: maxWidth ?? width),
+              child: Material(
+                type: MaterialType.transparency,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: backgroundColor ?? context.colors.surface,
+                    borderRadius: borderRadius,
+                  ),
+                  width: width,
+                  margin: margin,
+                  padding: padding,
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
-                    children: content,
+                    children: [
+                      Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Container(
+                              constraints: BoxConstraints(
+                                maxWidth: rowWidth,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: NomoText(
+                                  title,
+                                  style: titleStyle ??
+                                      context.theme.typographyTheme.h1,
+                                ),
+                              ),
+                            ),
+                          ),
+                          if (showCloseButton!)
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: closeButton ??
+                                    NomoButton.icon(
+                                      onPressed: Navigator.of(context).pop,
+                                      borderRadius: BorderRadius.circular(100),
+                                      icon: Icon(
+                                        Icons.close,
+                                      ),
+                                    ),
+                              ),
+                            ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          maxHeight: maxHeight ??
+                              MediaQuery.of(context).size.height * 0.5,
+                          minHeight: 0,
+                        ),
+                        child: content,
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      if (actions.isNotEmpty)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: actions,
+                        ),
+                    ],
                   ),
                 ),
               ),
-              Positioned(
-                left: 3,
-                bottom: 3,
-                right: 3,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: actions,
-                ),
-              ),
-            ],
-          ),
-        ),
+            ),
+          )),
+        ],
       ),
     );
   }
