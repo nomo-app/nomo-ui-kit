23dd66d7b5dc855a5d6a1ab3057cc2fd14233036
dev2-nomo <dev2@nomo.app>
2024-01-29 10:57:51 +0100

    added nomocolor to material

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:nomo_ui_kit/theme/sub/nomo_color_theme.dart';
 
 extension NotifierUtil<T> on ValueNotifier<T> {
   void update(T Function(T val) updater) {
@@ -11,5 +13,26 @@ extension NotifierUtil<T> on ValueNotifier<T> {
 extension ColorExtension on Color {
   String toHexColor() {
     return '0x${value.toRadixString(16).padLeft(8, '0')}';
+  }
+}
+
+extension NomoThemeExtension on NomoColors {
+  ThemeData get materialTheme {
+    return ThemeData(
+      primaryColor: primary,
+      colorScheme: ColorScheme(
+        primary: primary,
+        secondary: secondary,
+        background: background1,
+        surface: surface,
+        onBackground: foreground1,
+        onSurface: foreground1,
+        onPrimary: onPrimary,
+        onSecondary: onSecondary,
+        brightness: brightness,
+        error: error,
+        onError: foreground1,
+      ),
+    );
   }
 }
