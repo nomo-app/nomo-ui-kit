bc7103483904b2912b9b98d08da65df8d78feb72
dev2-nomo <dev2@nomo.app>
2024-02-19 14:46:45 +0100

    Use Decoration BorderRadius if BorderRadius not specified

--- lib/components/expandable/expandable.dart
+++ lib/components/expandable/expandable.dart
@@ -88,8 +88,9 @@ class _ExpandableState extends State<Expandable> with TickerProviderStateMixin {
 
   @override
   void initState() {
-    stateNotifier = widget.expansionNotifier ?? ValueNotifier(widget.initiallyExpanded)
-      ..addListener(onStateChanged);
+    stateNotifier =
+        widget.expansionNotifier ?? ValueNotifier(widget.initiallyExpanded)
+          ..addListener(onStateChanged);
 
     controller = AnimationController(
       vsync: this,
@@ -167,7 +168,10 @@ class _ExpandableState extends State<Expandable> with TickerProviderStateMixin {
                       splashColor: theme.splashColor,
                       hoverColor: theme.hoverColor,
                       focusColor: theme.focusColor,
-                      borderRadius: widget.borderRadius,
+                      borderRadius: widget.borderRadius ??
+                          widget.decoration?.borderRadius?.resolve(
+                            Directionality.of(context),
+                          ),
                       child: Padding(
                         padding: theme.titlePadding,
                         child: Row(
