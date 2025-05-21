dc48fdf8f596a6a21dbf8e061bebf22116c78268
dev2 <dev2@nomo.app>
2025-05-21 16:48:38 +0200

    Add LocaleResoulationCallback

--- lib/app/nomo_app.dart
+++ lib/app/nomo_app.dart
@@ -22,6 +22,7 @@ class NomoApp extends StatefulWidget {
     required this.routerConfig,
     required this.color,
     required this.supportedLocales,
+    this.localeResolutionCallback,
     super.key,
     this.localizationDelegate,
     this.currentLocale,
@@ -35,6 +36,7 @@ class NomoApp extends StatefulWidget {
   final LocalizationsDelegate<dynamic>? localizationDelegate;
   final Iterable<Locale> supportedLocales;
   final Locale? currentLocale;
+  final Locale? Function(Locale?, Iterable<Locale>)? localeResolutionCallback;
   final String Function(String value)? translator;
 
   /// A Wrapper that can access the ThemeProvider and NomoNavigator
@@ -67,11 +69,18 @@ class _NomoAppState extends State<NomoApp> {
         if (widget.localizationDelegate != null) widget.localizationDelegate!,
         GlobalMaterialLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
       ],
       supportedLocales: widget.supportedLocales,
       locale: widget.currentLocale,
+      localeResolutionCallback: widget.localeResolutionCallback,
       color: widget.color,
       routerConfig: widget.routerConfig,
+      builder: (context, child) {
+        if (widget.appWrapper != null)
+          return widget.appWrapper!(context, child!);
+        return child!;
+      },
     );
 
     return MultiWrapper(
@@ -90,12 +99,7 @@ class _NomoAppState extends State<NomoApp> {
         sizingThemeBuilder: widget.themeDelegate.sizingThemeBuilder,
         child: ThemeAnimator(
           notifier: themeNotifier,
-          child: app.wrapIf(
-            widget.appWrapper != null,
-            (metricReactor) => Builder(
-              builder: (context) => widget.appWrapper!(context, metricReactor),
-            ),
-          ),
+          child: app,
         ),
       ),
     );
